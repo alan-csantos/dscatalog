f609aad3da7e20e85f2ac13ce4c17e84895dc044
Alan Correia <csantos.alan@gmail.com>
2023-05-13 13:45:36 -0300

    atualização classes e resources

--- backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/com/devsuperior/dscatalog/DscatalogApplication.java
@@ -1,13 +1,13 @@
-package com.javastudy.dscatalog.backend;
+package com.devsuperior.dscatalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class DscatalogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(DscatalogApplication.class, args);
 	}
 
 }
